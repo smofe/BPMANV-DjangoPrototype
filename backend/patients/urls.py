87ab613f95639545808bd7c2306767983dde5990
Smofe <michel.klappert@student.hpi.de>
2020-10-15 10:12:43 +0200

    Refactored function based api views to class based (generic) api views

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('patients/', views.patient_list),
-    path('patients/<int:pk>/', views.patient_detail),
+    path('patients/', views.PatientList.as_view()),
+    path('patients/<int:pk>/', views.PatientDetail.as_view()),
     path('patients/<int:pk>/changestate/', views.patient_change_state),
     path('patients/<int:pk>/<str:field>/', views.patient_check_field),
-    path('patientstates/', views.patient_state_list),
-    path('patientstates/<int:pk>/', views.patient_state_detail),
+    path('patientstates/', views.PatientStateList.as_view()),
+    path('patientstates/<int:pk>/', views.PatientStateDetail.as_view()),
 ]
--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Patient, PatientState
@@ -9,93 +7,38 @@ from .serializers import PatientSerializer, PatientListSerializer, PatientStateS
 import datetime
 
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def patient_list(request):
+class PatientList(generics.ListCreateAPIView):
     """
     List all patients, or create a new patient.
     """
-    if request.method == 'GET':
-        patients = Patient.objects.all()
-        serializer = PatientListSerializer(patients, many=True)
-        return Response(serializer.data)
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
 
-    elif request.method == 'POST':
-        serializer = PatientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def patient_state_list(request):
+class PatientStateList(generics.ListCreateAPIView):
     """
     List all patient states, or create a new patient state.
     """
-    if request.method == 'GET':
-        patient_states = PatientState.objects.all()
-        serializer = PatientStateSerializer(patient_states, many=True)
-        return Response(serializer.data)
+    queryset = PatientState.objects.all()
+    serializer_class = PatientStateSerializer
 
-    elif request.method == 'POST':
-        serializer = PatientStateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def patient_detail(request, pk):
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Lists one patient in detail, update a patient or delete one.
     """
-    try:
-        patient = Patient.objects.get(pk=pk)
-    except Patient.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
 
-    if request.method == 'GET':
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = PatientSerializer(patient, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def patient_state_detail(request, pk):
+class PatientStateDetail(generics.RetrieveUpdateDestroyAPIView):
     """
         Lists one patient state in detail, update a patient state or delete one.
     """
-    try:
-        patient_state = PatientState.objects.get(pk=pk)
-    except PatientState.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    queryset = PatientState.objects.all()
+    serializer_class = PatientStateSerializer
 
-    if request.method == 'GET':
-        serializer = PatientStateSerializer(patient_state)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = PatientStateSerializer(patient_state, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        patient_state.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET'])
@@ -115,7 +58,7 @@ def patient_check_field(request, pk, field):
         current_time = current_date_and_time.strftime("%H:%M:%S")
         return Response({'finishing_time': current_time, field: serializer.data})
 
-@csrf_exempt
+
 @api_view(['PUT'])
 def patient_change_state(request,pk):
     """
