9360008e860f9143e851dffadec69c295ccb7d2a
Smofe <michel.klappert@student.hpi.de>
2020-10-22 12:03:28 +0200

    Refactored changestate view and changed next_states of patient_states to foreign fields. Implemented very basic logging function.

--- backend/patients/tasks.py
+++ backend/patients/tasks.py
@@ -1,6 +1,10 @@
 from celery import Celery
+from .models import Patient, PatientState
+from .serializers import PatientSerializer
+import datetime
 
 app = Celery()
+
 
 @app.task
 def test():
@@ -10,4 +14,14 @@ def test():
 @app.task
 def test2():
     print("Every 5 seconds...")
+
+@app.task
+def check_patient_states():
+    print("Checking state of all patients...")
+    patients = Patient.objects.all()
+    for patient in patients:
+        next_phase_timestamp = PatientSerializer(patient, context={'fields': ['next_phase_timestamp']}).data
+        print(next_phase_timestamp)
+        #print(datetime.now())
+    print(patients)
 
